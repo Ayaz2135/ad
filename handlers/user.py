c547de478f720284c165b6db57d6261ec29380e1
Ayaz2135 <168137282+Ayaz2135@users.noreply.github.com>
2025-10-08 16:24:50 +0530

    Delete handlers/user.py

--- handlers/user.py
+++
@@ -1,9 +0,0 @@
-# handlers/user.py
-from telegram import Update
-from telegram.ext import ContextTypes
-from database import add_user
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.effective_user
-    add_user(user.id, user.username, user.first_name)
-    await update.message.reply_text(f"👋 Hello {user.first_name}! Welcome to the Ad Bot.")
