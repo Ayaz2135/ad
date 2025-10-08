b4dff2446aa45e1ea672ab5aa66a8e736779225b
Ayaz2135 <168137282+Ayaz2135@users.noreply.github.com>
2025-10-08 16:22:56 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# Telegram Bot Token
+AD_BOT_TOKEN = "7398078402:AAGXTBxjuLt1q-4vhxKI0SbC3kmq2jQIFwY"
+
+# Telegram API Configuration
+API_ID = 29201178
+API_HASH = "4ec392f9e91855c0a99794a0e80c7fea"
+
+# Admin (your Telegram user ID)
+ADMIN_ID = 6103037379  # change to your real Telegram ID
